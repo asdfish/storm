aba745a92ac450b635ae88e271559d1b6d810e7c
asdfish <lgray3420@gmail.com>
2025-03-14 12:58:12 -0400

    rewrote logging

--- src/config.rs
+++ src/config.rs
@@ -1,31 +1,46 @@
-mod logger;
-
 use {
     opts::{Flag, Parser},
-    logger::{Logger, Null, Quiet, Verbose},
     std::{
+        cell::{RefCell, RefMut},
+        cmp::PartialOrd,
         ffi::{CStr, c_char, c_int},
-        process::{
-            Command,
-            exit,
-        },
+        fs::File,
+        io::{self, Write, stderr},
+        ops::DerefMut,
+        process::{Command, exit},
     },
 };
 
-pub struct Config<'a> {
-    commands: Vec<&'a str>,
-    logger: &'static dyn Logger,
+#[derive(Clone, Copy, Default, PartialEq, PartialOrd)]
+#[repr(u8)]
+pub enum LogLevel {
+    None,
+    #[default]
+    Quiet,
+    Verbose,
 }
-impl Default for Config<'_> {
-    fn default() -> Self {
-        Self {
-            commands: Vec::new(),
-            logger: &Quiet,
+impl LogLevel {
+    /// Compares the current log level with `level` and executes the function if the level is
+    /// higher than [self].
+    fn log<F: FnOnce(&mut dyn Write) -> io::Result<()>>(&self, level: Self, file: &mut dyn Write, print: F) {
+        if level >= *self {
+            if let Err(err) = print(file) {
+                eprintln!("error while logging: {}", err);
+            }
         }
     }
 }
+
+#[derive(Default)]
+pub struct Config<'a> {
+    commands: Vec<&'a str>,
+    log_level: LogLevel,
+    file: Option<RefCell<File>>,
+}
 impl<'a> Config<'a> {
     /// SAFETY: `argc` must be accurate and `argv` must point to owned memory addresses
+    ///
+    /// Errors in argument parsing are always printed to stderr.
     pub unsafe fn apply(&mut self, argc: c_int, argv: *const *const c_char) {
         if argc > 0 {
             let argv = (0..argc)
@@ -55,7 +70,7 @@ impl<'a> Config<'a> {
                         match parser.value(flag) {
                             Ok(value) => value,
                             Err(err) => {
-                                self.logger.error(&|| eprintln!("{}", err));
+                                eprintln!("{}", err);
                                 continue;
                             }
                         }
@@ -68,13 +83,15 @@ impl<'a> Config<'a> {
                             "usage: storm [OPTIONS]...
 
 Options:
-  -h --help    display this message and exit
-  -V --version show version information and exit
-  -v --verbose set how verbose logging should be
+  -h --help    Display this message and exit.
+  -V --version Show version information and exit.
+  -v --verbose Set how verbose logging should be:
                none    - disable logging
                quiet   - only log errors
                verbose - log progress
-  -c --command command to execute in wayland display"
+  -c --command Command to execute after initialization.
+  -l --log     File to print logs in.
+               Defaults to stderr if not set or printing fails."
                         );
 
                         exit(0);
@@ -84,18 +101,19 @@ Options:
                         exit(0);
                     }
                     Flag::Short('v') | Flag::Long("verbose") => match value_or_continue!() {
-                        "none" => self.logger = &Null,
-                        "quiet" => self.logger = &Quiet,
-                        "verbose" => self.logger = &Verbose,
-                        level => self
-                            .logger
-                            .error(&|| eprintln!("unknown verbosity level: `{}`", level)),
+                        "none" => self.log_level = LogLevel::None,
+                        "quiet" => self.log_level = LogLevel::Quiet,
+                        "verbose" => self.log_level = LogLevel::Verbose,
+                        level => eprintln!("unknown verbosity level: `{}`", level),
                     },
                     Flag::Short('c') | Flag::Long("command") => {
                         self.commands.push(value_or_continue!());
                     }
+                    Flag::Short('l') | Flag::Long("log") => {
+                        self.file = File::create(value_or_continue!()).map(RefCell::new).ok();
+                    }
                     flag @ Flag::Short(_) | flag @ Flag::Long(_) => {
-                        self.logger.error(&|| eprintln!("unknown flag `{}`", flag));
+                        eprintln!("unknown flag `{}`", flag);
                     }
                     _ => {}
                 }
@@ -104,10 +122,22 @@ Options:
     }
 
     pub fn execute_commands(&self) {
-        self.commands.iter()
+        self.commands
+            .iter()
             .for_each(|command| match Command::new(command).spawn() {
-                Ok(_) => {},
-                Err(err) => self.logger.error(&|| eprintln!("error spawning command `{}`: {}", command, err)),
+                Ok(_) => {}
+                Err(err) => self.log(LogLevel::Quiet, |f| writeln!(f, "error spawning command `{}`: {}", command, err))
             })
+    }
+
+    pub fn log<F: FnOnce(&mut dyn Write) -> io::Result<()>>(&self, level: LogLevel, print: F) {
+        match &self.file {
+            Some(file) => self.log_level.log(
+                level,
+                <RefMut<'_, File> as DerefMut>::deref_mut(&mut file.borrow_mut()),
+                print,
+            ),
+            None => self.log_level.log(level, &mut stderr(), print),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 #![no_main]
 
-mod attempt;
 mod config;
-mod state;
 
 use {
-    attempt::{Always, Attempt, DEFAULT_ATTEMPTS},
     config::Config,
-    state::Storm,
-    smithay::reexports::calloop::EventLoop,
-    std::{
-        ffi::{c_char, c_int},
-        time::Duration,
-    },
+    std::ffi::{c_char, c_int},
 };
 
 // SAFETY: every c program has done this since the dawn of time
@@ -21,20 +13,6 @@ fn main(argc: c_int, argv: *const *const c_char) -> c_int {
     let mut config = Config::default();
     // SAFETY: argc and argv should not be unsafe to dereference
     unsafe { config.apply(argc, argv) };
-
-    let mut event_loop = EventLoop::<Storm>::try_new().unwrap();
-    let mut storm = Storm::new(&event_loop);
-
-    //config.execute_commands();
-
-    //event_loop.run(
-    //    None,
-    //    //Some(Duration::from_millis(10)),
-    //    &mut storm,
-    //    |_data| {
-    //        println!("something happened");
-    //    }
-    //).unwrap();
 
     0
 }
