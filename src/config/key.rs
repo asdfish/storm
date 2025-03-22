ed16a6765b0f6d161c42473e096b391ae055fd5a
asdfish <lgray3420@gmail.com>
2025-03-22 06:02:11 -0400

    started key binding parser

--- src/config/key.rs
+++ src/config/key.rs
@@ -8,6 +8,10 @@ use {
     },
 };
 
+pub trait Parser<'a>: Sized {
+    fn parse(_: &'a str) -> Result<(Self, &'a str), ParserError<'a>>;
+}
+
 #[derive(Enum)]
 pub enum KeyAction {
     Quit,
@@ -24,8 +28,9 @@ impl Display for Key<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match &self.kind {
             KeyKind::Invisible(key) => write!(f, "{}{}", self.mods, key),
-            KeyKind::Visible(keys) => keys.chars()
-                .try_for_each(|key| write!(f, "{}{}", self.mods, key))
+            KeyKind::Visible(keys) => keys
+                .chars()
+                .try_for_each(|key| write!(f, "{}{}", self.mods, key)),
         }
     }
 }
@@ -41,20 +46,10 @@ impl Display for KeyKind<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Self::Invisible(key) => write!(f, "{}", key),
-            Self::Visible(key) => {
-                key.chars()
-                    .try_for_each(|ch| {
-                        match ch {
-                            'M' |
-                            'C' |
-                            'S' |
-                            'L' |
-                            '<' |
-                            '>' => write!(f, "\\{}", ch),
-                            ch => write!(f, "{}", ch),
-                        }
-                    })
-            }
+            Self::Visible(key) => key.chars().try_for_each(|ch| match ch {
+                'M' | 'C' | 'S' | 'L' | '<' | '>' => write!(f, "\\{}", ch),
+                ch => write!(f, "{}", ch),
+            }),
         }
     }
 }
@@ -68,6 +63,20 @@ impl<'a> From<&'a str> for KeyKind<'a> {
         Self::Visible(key.into())
     }
 }
+impl<'a> Parser<'a> for KeyKind<'a> {
+    fn parse(input: &'a str) -> Result<(Self, &'a str), ParserError<'a>> {
+        if input.starts_with("<") {
+            InvisibleKey::parse(input)
+                .map(|(key, out)| (Self::Invisible(key), out))
+        } else {
+            let mut out = Cow::Borrowed("");
+            let mut chars = input.char_indices();
+
+            todo!()
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum InvisibleKey {
     /// Function keys
@@ -86,6 +95,33 @@ impl Display for InvisibleKey {
         }?;
 
         write!(f, ">")
+    }
+}
+impl<'a> Parser<'a> for InvisibleKey {
+    fn parse(input: &'a str) -> Result<(Self, &'a str), ParserError<'a>> {
+        if !input.starts_with('<') {
+            Err(ParserError::None)
+        } else if let Some(end) = input.find('>') {
+            match &input[1..end] {
+                "F" => {
+                    let fkey = input[2..].chars().try_fold(0_u8, |fold, ch| {
+                        let err = || ParserError::UnknownSpecialKey(&input[1..end]);
+
+                        fold.checked_mul(10)
+                            .ok_or_else(err)?
+                            .checked_add(ch.to_digit(10).ok_or_else(err)? as u8)
+                            .ok_or_else(err)
+                    })?;
+
+                    Ok((Self::F(fkey), &input[end + 1..]))
+                }
+                "PG-UP" => Ok((Self::PageUp, &input[end + 1..])),
+                "PG-DN" => Ok((Self::PageDown, &input[end + 1..])),
+                key => Err(ParserError::UnknownSpecialKey(key)),
+            }
+        } else {
+            Err(ParserError::UnclosedSpecialKey(input))
+        }
     }
 }
 
@@ -176,31 +212,80 @@ impl Display for KeyModifier {
         }
     }
 }
+impl<'a> Parser<'a> for KeyModifier {
+    fn parse(input: &'a str) -> Result<(Self, &'a str), ParserError<'a>> {
+        macro_rules! parse_modifiers {
+            ($(($head:expr, $modifier:expr)),* $(,)?) => {
+                match input {
+                    $(input if input.starts_with($head) => Ok(($modifier, &input[const {
+                        $head.len()
+                    }..])),)*
+                    _ => Err(ParserError::None),
+                }
+            }
+        }
+
+        parse_modifiers![
+            ("M-", Self::Alt),
+            ("C-", Self::Control),
+            ("S-", Self::Shift),
+            ("L-", Self::Super),
+        ]
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub struct KeyModifiers(EnumMap<KeyModifier, bool>);
 impl KeyModifiers {
+    pub fn new() -> Self {
+        Self::from_fn(|_| false)
+    }
+
     pub fn from_fn<F>(f: F) -> Self
     where
-        F: FnMut(KeyModifier) -> bool {
+        F: FnMut(KeyModifier) -> bool,
+    {
         Self(EnumMap::from_fn(f))
     }
 
     /// Returns whether there are any active key modifiers
     pub fn is_active(&self) -> bool {
-        self.0.values()
-            .copied()
-            .any(|active| active)
+        self.0.values().copied().any(|active| active)
+    }
+
+    pub fn push(&mut self, modifier: KeyModifier) {
+        self.0[modifier] = true;
     }
 }
 impl Display for KeyModifiers {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        self
-            .0
+        self.0
             .iter()
             .filter_map(|(modifier, active)| active.then_some(modifier))
             .try_for_each(|modifier| write!(f, "{}", modifier))
     }
+}
+impl<'a> Parser<'a> for KeyModifiers {
+    fn parse(mut input: &'a str) -> Result<(Self, &'a str), ParserError<'a>> {
+        let mut output = Self::new();
+        while let Ok((modifier, next_input)) = KeyModifier::parse(input) {
+            output.push(modifier);
+            input = next_input;
+        }
+
+        if output.is_active() {
+            Ok((output, input))
+        } else {
+            Err(ParserError::None)
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ParserError<'a> {
+    None,
+    UnknownSpecialKey(&'a str),
+    UnclosedSpecialKey(&'a str),
 }
 
 #[cfg(test)]
@@ -211,10 +296,16 @@ mod tests {
     fn key_sequence_contains() {
         assert!(
             KeySequence::from_iter([
-                Key { kind: "foo".into(), mods: KeyModifiers::from_fn(|_| false) },
-                Key { kind: "bar".into(), mods: KeyModifiers::from_fn(|_| true) },
+                Key {
+                    kind: "foo".into(),
+                    mods: KeyModifiers::from_fn(|_| false)
+                },
+                Key {
+                    kind: "bar".into(),
+                    mods: KeyModifiers::from_fn(|_| true)
+                },
             ])
-            .contains(&KeySequence::from_iter([Key{
+            .contains(&KeySequence::from_iter([Key {
                 kind: "foo".into(),
                 mods: KeyModifiers::from_fn(|_| false)
             }]))
@@ -225,14 +316,32 @@ mod tests {
     fn key_sequence_flatten() {
         assert_eq!(
             KeySequence::from_iter([
-                Key { kind: "foo".into(), mods: KeyModifiers::from_fn(|_| false) },
-                Key { kind: "bar".into(), mods: KeyModifiers::from_fn(|_| false) },
-                Key { kind: "foo".into(), mods: KeyModifiers::from_fn(|_| true) },
-                Key { kind: "bar".into(), mods: KeyModifiers::from_fn(|_| true) },
+                Key {
+                    kind: "foo".into(),
+                    mods: KeyModifiers::from_fn(|_| false)
+                },
+                Key {
+                    kind: "bar".into(),
+                    mods: KeyModifiers::from_fn(|_| false)
+                },
+                Key {
+                    kind: "foo".into(),
+                    mods: KeyModifiers::from_fn(|_| true)
+                },
+                Key {
+                    kind: "bar".into(),
+                    mods: KeyModifiers::from_fn(|_| true)
+                },
             ]),
             KeySequence::from_iter([
-                Key { kind: "foobar".into(), mods: KeyModifiers::from_fn(|_| false) },
-                Key { kind: "foobar".into(), mods: KeyModifiers::from_fn(|_| true) },
+                Key {
+                    kind: "foobar".into(),
+                    mods: KeyModifiers::from_fn(|_| false)
+                },
+                Key {
+                    kind: "foobar".into(),
+                    mods: KeyModifiers::from_fn(|_| true)
+                },
             ])
         );
     }
